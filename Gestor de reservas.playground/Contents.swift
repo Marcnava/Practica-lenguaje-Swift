3445659e0401931195ecb4a3834df298b2e2bb37
Marc Navarro <cram.nomura@gmail.com>
2023-07-09 16:09:13 +0200

    Añado estructuras y clases básicas a falta de desarrollarlas

--- Gestor de reservas.playground/Contents.swift
+++ Gestor de reservas.playground/Contents.swift
@@ -1,3 +1,68 @@
 import Foundation
 
+struct Client {
+    
+    let name: String
+    let age: UInt
+    let height: Float
+    
+}
 
+struct Reservation {
+    
+    let id: UInt = 1
+    let nameHotel: String = "Fabulous Hotel"
+    let clientList: [String]
+    let days: UInt
+    let price: Decimal
+    let breakfast: Bool
+    
+}
+
+enum ReservationError: Error {
+    
+    case idMatch
+    case clientMatch
+    case noReservation
+    
+}
+
+class HotelReservationManager {
+    
+    var reservationList: [Reservation]
+    
+    init(reservationList: [Reservation]) {
+        self.reservationList = reservationList
+    }
+    
+    func addReservation(_ clientList: [String],_ days: UInt,_ breakfast: Bool) {
+        
+    }
+    
+    func removeReservation(_ id: UInt) {
+        
+    }
+    
+    func showReservationList(_ reservationList: [Reservation]) {
+        if(reservationList.count > 0) {
+            for reservation in reservationList {
+                print(reservation)
+            }
+        } else {
+            print("No hay reservas.")
+        }
+    }
+}
+
+
+func testAddReservation() {
+    
+}
+
+func testCancelReservation() {
+    
+}
+
+func testReservationPrice() {
+    
+}
